525c13587ac4b5d7da250c27944cbf3d4129757d
Abiyasa <abiyasa2410@gmail.com>
2023-06-15 20:43:36 +0700

    feat(majicmix-realistic): v6

---
+++ download.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+mkdir_wget(){
+    mkdir -p $1
+    wget -nc -P $1 --content-disposition $2
+}
+
+# EMBEDDINGS / TEXTUAL INVERSION
+# easynegative.safetensors
+mkdir_wget embeddings https://civitai.com/api/download/models/9208
+# ng_deepnegative_v1_75t.pt
+mkdir_wget embeddings https://civitai.com/api/download/models/5637
+# verybadimagenegative_v1.3.pt
+mkdir_wget embeddings https://civitai.com/api/download/models/25820
+
+# LORA
+# add_detail.safetensors
+mkdir_wget models/Lora https://civitai.com/api/download/models/62833
+# more_details.safetensors
+mkdir_wget models/Lora https://civitai.com/api/download/models/87153
+# skirtlift-v1.safetensors
+mkdir_wget models/Lora https://civitai.com/api/download/models/10187
+
+# MODELS
+mkdir -p models/Stable-diffusion 
+# majicmixRealistic_v6.safetensors
+mkdir_wget models/Stable-diffusion https://civitai.com/api/download/models/94640
