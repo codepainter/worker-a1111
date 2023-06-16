b76d03cd4a75b9a7470d72a1b687e36f88dfc7c7
Abiyasa <abiyasa2410@gmail.com>
2023-06-16 13:44:12 +0700

    feat(majicmix-realistic): fix build script

--- images/majicmixRealistic_v6/build.sh
+++ images/majicmixRealistic_v6/build.sh
@@ -6,7 +6,7 @@
 ./images/majicmixRealistic_v6/download.sh
 
 docker build \
-        -f images/majicmixrealistic_v6/Dockerfile \
+        -f images/majicmixRealistic_v6/Dockerfile \
         -t abiyasa2410/sd-worker:majicmix-realistic-v6 \
         .
 
