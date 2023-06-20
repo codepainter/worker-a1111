4367a075b61d71f427f48d86e23e2c73c42457b6
Abiyasa <abiyasa2410@gmail.com>
2023-06-20 22:46:04 +0700

    feat(majicmix-realistic): refactor build script

--- build.sh
+++ build.sh
@@ -3,23 +3,23 @@
 # run from root dir 
 # eg: ./build.sh majicmix-realistic-v6
 
-BRANCH_NAME=$1
+# BRANCH_NAME=$1
 
-echo "Checking out and pulling branch..."
-git checkout $BRANCH_NAME && git pull
+# echo "Checking out and pulling branch..."
+# git checkout $BRANCH_NAME && git pull
 
-FOLDERNAME=$1
+# FOLDERNAME=$1
 
-echo "Downloading files..."
-./images/$FOLDERNAME/download.sh
+# echo "Downloading files..."
+# ./images/$FOLDERNAME/download.sh
 
-REPO_NAME=abiyasa2410/worker-a1111
-TAG=$1
+REPO_NAME=$1
+TAG=$2
 FULL_IMAGE_NAME=$REPO_NAME:$TAG
 
 echo "Building $FULL_IMAGE_NAME..."
 docker build \
-        -f images/$1/Dockerfile \
+        -f images/$2/Dockerfile \
         -t $FULL_IMAGE_NAME \
         .
 
